969286348bf3bcb6b35d34f83805047ed39a01a4
jxytherealone <thushar17223@gmail.com>
2023-11-24 14:19:46 +0530

    registration form is complete

--- php/register.php
+++ php/register.php
@@ -9,17 +9,28 @@
         $gender = $_POST['gender'];
         $dob = date('Y-m-d', strtotime($_POST['dob']));
         $password = md5($_POST['password']);
-
-        $sql = "INSERT INTO users (firstname, lastname, email, phonenumber, dob, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $checkemail = $_POST['email'];
+        
+        $sql = "SELECT email FROM users WHERE email = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("sssssss", $firstname, $lastname, $email, $phonenumber, $dob, $gender, $password);
-        $result = $stmt->execute();
-        if ($result == TRUE) {
-            echo '<script>alert("successfully created your account");</script>';
-            echo '<script>window.location.href="home.html";</script>';
+        $stmt->bind_param("s", $checkemail);
+        $out = $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            echo '<script>alert("this email already exists");</script>';
+            echo '<script>window.location.href="../register.html";</script>';
         } else {
-            echo 'something went wrong' . $conn->error;
-            echo 'something went wrong' . $stmt->error;
+            $sql = "INSERT INTO users (firstname, lastname, email, phonenumber, dob, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssssss", $firstname, $lastname, $email, $phonenumber, $dob, $gender, $password);
+            $result = $stmt->execute();
+            if ($result == TRUE) {
+                echo '<script>alert("successfully created your account");</script>';
+                echo '<script>window.location.href="home.html";</script>';
+            } else {
+                echo 'something went wrong' . $conn->error;
+                echo 'something went wrong' . $stmt->error;
+            }
         }
     }
 
