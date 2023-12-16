1311900329e98348c6b1f8ad111af07055884ea6
jxytherealone <thushar17223@gmail.com>
2023-12-16 18:28:18 +0530

    some small indentations

--- backend-php/connect.php
+++ backend-php/connect.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "",  "stress.db.dev");
-if (!$conn) {
-    echo 'something went wrong ' . mysqli_connect_error();
-}
+    $conn = mysqli_connect("localhost", "root", "",  "stress.db.dev");
+    if (!$conn) {
+        echo 'something went wrong ' . mysqli_connect_error();
+    }
 ?>
