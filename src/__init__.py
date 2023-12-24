43fabb874adbc9a2bc73dee5e3879004001d5ec3
jxytherealone <thushar17223@gmail.com>
2023-12-24 11:14:11 +0530

    rewrote the login-registration from scratch and now feeling pain in the butt

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 import mysql.connector
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'VERYCOOLKEY'
 conn = mysql.connector.connect(host='localhost', user='root', password='', db='stress.db.dev')
 
 cursor = conn.cursor(dictionary=True)
 
 from . import register
+from . import login
--- src/register.py
+++ src/register.py
@@ -1,6 +1,24 @@
 from . import app, conn, cursor
-from flask import session, url_for, redirect, render_template
+from flask import session, url_for, redirect, render_template, request
+import base64
 
-@app.route('/usr/register')
+@app.route('/usr/register', methods = ['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		first_name = request.form['first_name']
+		last_name = request.form['last_name']
+		email = request.form['email']
+		phone_number = request.form['phone_number']
+		dob = request.form['dob']
+		gender = request.form['gender']
+
+		encoding_string = request.form['password']
+		encoded_string = encoding_string.encode('ascii')
+		password = base64.b64encode(encoded_string)
+
+		sql = "INSERT INTO users (firstname, lastname, email, phonenumber, dob, gender, password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+		values = (first_name, last_name, email, phone_number, dob, gender, password,)
+		cursor.execute(sql, values)
+		conn.commit()
+		return redirect(url_for('login'))
 	return render_template('register.html')
