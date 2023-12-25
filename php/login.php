3e28aeeec8f6db2f529d526fd88b40d2dcdbe802
jxytherealone <thushar17223@gmail.com>
2023-12-25 10:52:51 +0530

    i hate indentation

--- php/login.php
+++ php/login.php
@@ -1,27 +1,27 @@
 <?php 
-include('connect.php');
+    include('connect.php');
 
-if (isset($_POST['login'])) {
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
 
-    $sql = "SELECT email, password FROM users WHERE email = ? AND password = ?";
-    $stmt = $conn->prepare($sql);
+        $sql = "SELECT email, password FROM users WHERE email = ? AND password = ?";
+        $stmt = $conn->prepare($sql);
 
-    $stmt->bind_param("ss", $email, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        $stmt->bind_param("ss", $email, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-    if ($result->num_rows > 0) {
-        echo '<script>alert("Successfully logged in"); document.location.href="../home.html";</script>';
-    } else  {
-        echo '<script>alert("This account does not exist"); document.location.href="../login.html";</script>';
+        if ($result->num_rows > 0) {
+            echo '<script>alert("Successfully logged in"); document.location.href="../home.html";</script>';
+        } else  {
+            echo '<script>alert("This account does not exist"); document.location.href="../login.html";</script>';
+        }
+
+        $stmt->close();
+    } else {
+        echo 'Something went wrong: ' . $conn->error;
     }
 
-    $stmt->close();
-} else {
-    echo 'Something went wrong: ' . $conn->error;
-}
-
-$conn->close();
+    $conn->close();
 ?>
