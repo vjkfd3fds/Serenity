0e433a5e38723e40b7fbfdd7e2be9ef070cf1a90
jxytherealone <thushar17223@gmail.com>
2024-01-12 21:03:15 +0530

    fixed styling with the generate_account.php

--- admin/generate_account.php
+++ admin/generate_account.php
@@ -1,10 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+</head>
+<body>
+	<div class="container mt-5">
+		<form method="POST" action="generate_account.php">
+			<input type="submit" name="generate" value="Create Account" />
+		</form>
+		<h2 class="mt-4">Generated Accounts</h2>
+
 <?php 
+	error_reporting(0);
+
 	include_once '../php/config.php';
 
 	function rand_password_generator() {
 		$alph = 'abcdefghijlmnopqrstuvwxyz1234567890!@#$%^&*()_-+=';
 		$rand_string = '';
-		for ($i=0; $i<10; $i++) {
+		for ($i=1; $i<5; $i++) {
 			$rand_string .= $alph[rand(0, strlen($alph))];
 		}
 		return $rand_string;
@@ -25,33 +43,32 @@
 		}
 
 	}
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
-	<form method="POST" action="generate_account.php">
-		<input type="submit" name="generate" value="Create Account" />
-	</form>
-</body>
-</html>
-
-<?php 
 	
 	$sql = "SELECT * FROM doctor_login";
 	$result = $conn->query($sql);
+	echo '<table class="table table-bordered mt-3">';
+	echo '<thead class="thead-dark">';
+	echo '<tr>';
+	echo '<th scope="col">Username</th>';
+	echo '<th scope="col">Password</th>';
+	echo '</tr>';
+	echo '</thead>';
+	echo '<tbody>';
 
-	while ($row = $result->fetch_assoc()) {
-		echo 'Username: ' . $row['username'];
-		echo '<br />';
-		echo 'Password ' . $row['password'];
-		echo '<br />';
-	} 
-
-
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			echo '<tr>';
+			echo '<td>' . $row['username'] . '</td>';
+			echo '<td>' . $row['password'] . '</td>';
+			echo '</tr>';
+		}
+		echo '</tbody>';
+        echo '</table>'; 
+	} else {
+		echo "NO data found";
+	}
 ?>
+</div>
+</body>
+</html>
