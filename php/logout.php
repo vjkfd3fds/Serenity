78763ec87dc99d7800a5e3a92bfb5f201b354b78
jxytherealone <thushar17223@gmail.com>
2024-02-01 12:00:58 +0530

    WOW SO COOl

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,8 @@
 <?php 
 	if (isset($_COOKIE['uid'])) {
 		$uid = $_COOKIE['uid'];
+		setcookie("uid", $uid, time() - 3600, "/");
+		echo '<script>alert("successfully logged out!!"); window.location.href="../index.php"</script>';
 	}
-	setcookie("uid", $uid, time() - 3600, "/");
 
 ?>
---
+++ user/home.php
@@ -0,0 +1,28 @@
+<?php
+	include ('../php/config.php'); 
+	if (isset($_COOKIE['uid'])) {
+		$uid = $_COOKIE['uid'];
+	}
+
+	$sql = "SELECT * FROM users WHERE id = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("i", $uid);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$row = $result->fetch_assoc();
+?>
+
+
+<h1><?php echo "logged in as " . $row['firstname'];?></h1>
+
+<div>
+	<ul>
+		<li><a href="songs.php">Play songs</a>
+		<li><a href="books.php">Read books</a>
+		<li><a href="stress_calculate.php">Calculate stress (current stress)</a>
+		<li><a href="chat.php">Chat with a bot</a>
+		<li><a href="writing.php">Writing board</a>
+		<li><a href="yoga.php">Yoga</a>
+		<li><a href="../php/logout.php">Logout</a>
+	</ul>
+</div>
