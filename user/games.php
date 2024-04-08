ad017b618d5210b153e981f7ac15b3b0a27cc8af
jxytherealone <thushar17223@gmail.com>
2024-04-08 13:29:09 +0530

    YOY

--- user/games.php
+++ user/games.php
@@ -15,7 +15,7 @@
         <img src="games/images/flappy.gif" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Clumsy Bird</h5>
-          <p class="card-text">Description of the game goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">Clumsy Bird is a delightful mobile game where players navigate a charming bird through a series of obstacles with simple taps on their screens. The goal is to keep the bird flying for as long as possible without crashing into pipes or the ground. With its vibrant graphics and easy-to-learn gameplay.</p>
           <a href="games/clumsy-bird-master/index.html" class="btn btn-primary">Play Now</a>
         </div>
       </div>
@@ -25,7 +25,7 @@
         <img src="games/images/color-picker.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Color Picker</h5>
-          <p class="card-text">Description of the game goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">Color Picker is a handy tool designed to help users select colors from a digital palette. Whether you're a designer, artist, or simply want to find the perfect hue for your project. </p>
           <a href="games/color-picker/color.html" class="btn btn-primary">Play Now</a>
         </div>
       </div>
@@ -35,7 +35,7 @@
         <img src="games/images/snake.gif" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Snake Game</h5>
-          <p class="card-text">Description of the game goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">The Snake Game is a classic arcade game where players control a snake that grows longer as it consumes food items, all while avoiding colliding with obstacles or its own tail. </p>
           <a href="games/snake-gamecss-renderer/dist/index.html" class="btn btn-primary">Play Now</a>
         </div>
       </div>
@@ -45,7 +45,7 @@
         <img src="games/images/brain.gif" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Memory Game</h5>
-          <p class="card-text">Description of the game goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">The Memory Game is a delightful brain-teaser where players test their recall skills by flipping cards to find matching pairs. </p>
           <a href="games/memory-game/memory.php" class="btn btn-primary">Play Now</a>
         </div>
       </div>
@@ -55,7 +55,8 @@
         <img src="games/images/tower.gif" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Tower Blocks</h5>
-          <p class="card-text">Description of the game goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <p class="card-text">
+Tower Blocks is an addictive and fun puzzle game where players aim to build the tallest tower possible by stacking blocks on top of each other. With simple controls, players must time their taps accurately to place each block perfectly atop the previous one. </p>
           <a href="games/tower-blocks/dist/index.html" class="btn btn-primary">Play Now</a>
         </div>
       </div>
