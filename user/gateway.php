9bdb19450c7c6aab7434aa61933cdf42eef9f30d
jxytherealone <thushar17223@gmail.com>
2024-04-11 13:15:07 +0530

    WoW

--- user/gateway.php
+++ user/gateway.php
@@ -311,6 +311,10 @@
 	        font-size: 9px;
 	    }
 	}
+
+	.btn-primary input[type="submit"] {
+        border: none;
+    }
 	</style>
 </head>
 <body>
@@ -330,57 +334,43 @@
                         <p class="dis fw-bold mb-2">Card details</p>
                         <div class="d-flex align-items-center justify-content-between card-atm border rounded">
                             <div class="fab fa-cc-visa ps-3"></div>
-                            <input type="text" class="form-control" placeholder="Card Details">
+                            <input type="text" class="form-control" placeholder="Card Details" required>
                             <div class="d-flex w-50">
-                                <input type="text" class="form-control px-0" placeholder="MM/YY">
-                                <input type="password" maxlength=3 class="form-control px-0" placeholder="CVV">
+                                <input type="text" class="form-control px-0" placeholder="MM/YY" required>
+                                <input type="password" maxlength=3 class="form-control px-0" placeholder="CVV" required>
                             </div>
                         </div>
                         <div class="my-3 cardname">
                             <p class="dis fw-bold mb-2">Cardholder name</p>
-                            <input class="form-control" type="text">
+                            <input class="form-control" type="text" required>
                         </div>
                         <div class="address">
                             <p class="dis fw-bold mb-3">Billing address</p>
-                            <select class="form-select" aria-label="Default select example">
+                            <select class="form-select" aria-label="Default select example" required>
                                 <option selected hidden>United States</option>
                                 <option value="1">India</option>
-                                <option value="2">Australia</option>
-                                <option value="3">Canada</option>
                             </select>
                             <div class="d-flex">
-                                <input class="form-control zip" type="text" placeholder="ZIP">
-                                <input class="form-control state" type="text" placeholder="State">
+                                <input class="form-control zip" type="text" placeholder="ZIP" required>
+                                <input class="form-control state" type="text" placeholder="State" required>
                             </div>
                             <div class=" my-3">
-                                <p class="dis fw-bold mb-2">VAT Number</p>
+                                <p class="dis fw-bold mb-2">Enter the amount</p>
                                 <div class="inputWithcheck">
-                                    <input class="form-control" type="text" value="GB012345B9">
+                                    <input class="form-control" type="text" value="" required>
                                     <span class="fas fa-check"></span>
 
                                 </div>
                             </div>
-                            <div class="d-flex flex-column dis">
-                                <div class="d-flex align-items-center justify-content-between mb-2">
-                                    <p>Subtotal</p>
-                                    <p><span class="fas fa-dollar-sign"></span>33.00</p>
-                                </div>
-                                <div class="d-flex align-items-center justify-content-between mb-2">
-                                    <p>VAT<span>(20%)</span></p>
-                                    <p><span class="fas fa-dollar-sign"></span>2.80</p>
-                                </div>
-                                <div class="d-flex align-items-center justify-content-between mb-2">
-                                    <p class="fw-bold">Total</p>
-                                    <p class="fw-bold"><span class="fas fa-dollar-sign"></span>35.80</p>
-                                </div>
-                                <div class="btn btn-primary mt-2">Pay<span class="fas fa-dollar-sign px-1"></span>35.80
-                                </div>
+                                <div class="btn btn-primary mt-2">
+    								<input type="submit" onclick="alert('payment successful!'); return false;" value="Pay"><span class="fas fa-dollar-sign px-1"></span>
+								</div>
                             </div>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-    </div>s
+    </div>
 </body>
 </html>
